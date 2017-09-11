7f00cb4c1aad88aeadbf308fbae01e0fe92573e4
codeface <i@codeface.cn>
2017-09-11 17:27:40 +0800

    change base unit from mQtum to Qtum

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -119,7 +119,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         self.create_status_bar()
         self.need_update = threading.Event()
 
-        self.decimal_point = config.get('decimal_point', 5)
+        self.decimal_point = config.get('decimal_point', 8)
         self.num_zeros     = int(config.get('num_zeros',0))
 
         self.completions = QStringListModel()
