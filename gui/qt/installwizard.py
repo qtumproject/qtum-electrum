c67847a1e98549b18a85c0c37a37c2d8c7f0cd23
CodeFace <i@codeface.cn>
2017-10-28 13:52:10 +0800

    change version he install title

--- gui/qt/installwizard.py
+++ gui/qt/installwizard.py
@@ -106,7 +106,7 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
     def __init__(self, config, app, plugins, storage):
         BaseWizard.__init__(self, config, storage)
         QDialog.__init__(self, None)
-        self.setWindowTitle('Electrum  -  ' + _('Install Wizard'))
+        self.setWindowTitle('Qtum Electrum  -  ' + _('Install Wizard'))
         self.app = app
         self.config = config
         # Set for base base class
