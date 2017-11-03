21d17eec61aa3634dd7188d51c9c3d61843e9c04
CodeFace <i@codeface.cn>
2017-11-03 10:51:22 +0800

    [follow] fix magic number for p2wsh-p2sh follow https://github.com/spesmilo/electrum/commit/8623d8c258cf48662bbb10413412de82b81e33b8

--- lib/qtum.py
+++ lib/qtum.py
@@ -57,14 +57,14 @@ RECOMMEND_CONFIRMATIONS = 10
 XPRV_HEADERS = {
     'standard': 0x0488ade4,
     'p2wpkh-p2sh': 0x049d7878,
-    'p2wsh-p2sh': 0x295b004,
+    'p2wsh-p2sh': 0x295b005,
     'p2wpkh': 0x4b2430c,
     'p2wsh': 0x2aa7a99
 }
 XPUB_HEADERS = {
     'standard': 0x0488b21e,
     'p2wpkh-p2sh': 0x049d7cb2,
-    'p2wsh-p2sh': 0x295b43e,
+    'p2wsh-p2sh': 0x295b43f,
     'p2wpkh': 0x4b24746,
     'p2wsh': 0x2aa7ed3
 }
@@ -89,14 +89,14 @@ def set_testnet():
     XPRV_HEADERS = {
         'standard': 0x04358394,
         'p2wpkh-p2sh': 0x049d7878,
-        'p2wsh-p2sh': 0x295b004,
+        'p2wsh-p2sh': 0x295b005,
         'p2wpkh': 0x4b2430c,
         'p2wsh': 0x2aa7a99
     }
     XPUB_HEADERS = {
         'standard': 0x043587CF,
         'p2wpkh-p2sh': 0x049d7cb2,
-        'p2wsh-p2sh': 0x295b43e,
+        'p2wsh-p2sh': 0x295b43f,
         'p2wpkh': 0x4b24746,
         'p2wsh': 0x2aa7ed3
     }
