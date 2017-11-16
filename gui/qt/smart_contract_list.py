ce7ab0358a9d600968a64fd81d37bfe30d6a7686
CodeFace <i@codeface.cn>
2017-11-16 18:00:35 +0800

    contact list and smart contract list set stretch_column to None

--- gui/qt/smart_contract_list.py
+++ gui/qt/smart_contract_list.py
@@ -6,7 +6,6 @@ __author__ = 'CodeFace'
 """
 import webbrowser
 from electrum.i18n import _
-from electrum.qtum import is_address
 from electrum.util import block_explorer_URL, format_satoshis, format_time, age
 from electrum.plugins import run_hook
 from PyQt5.QtGui import *
@@ -19,7 +18,7 @@ class SmartContractList(MyTreeWidget):
     filter_columns = [0, 1]  # Key, Value
 
     def __init__(self, parent):
-        MyTreeWidget.__init__(self, parent, self.create_menu, [_('Name'), _('Address')], 0, [0])
+        MyTreeWidget.__init__(self, parent, self.create_menu, [_('Name'), _('Address'), _('Type')], None, [0])
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.setSortingEnabled(True)
 
@@ -52,8 +51,8 @@ class SmartContractList(MyTreeWidget):
         current_key = item.data(0, Qt.UserRole) if item else None
         self.clear()
         for address in sorted(self.parent.smart_contracts.keys()):
-            _type, name, abi = self.parent.smart_contracts[address]
-            item = QTreeWidgetItem([name, address])
+            name, _type, abi = self.parent.smart_contracts[address]
+            item = QTreeWidgetItem([name, address, _type])
             item.setData(0, Qt.UserRole, address)
             self.addTopLevelItem(item)
             if address == current_key:
