e7e26da7eaec493cab4066d90f66dbb9a1d06a9f
CodeFace <i@codeface.cn>
2017-12-14 14:05:39 +0800

    don't use urllib2 follow https://github.com/spesmilo/electrum/commit/07629e12afdabc359a34917d3df002021f26d0b2

--- lib/commands.py
+++ lib/commands.py
@@ -35,7 +35,7 @@ from functools import wraps
 from decimal import Decimal
 
 from .import util
-from .util import print_msg, format_satoshis, print_stderr
+from .util import bfh, bh2u, format_satoshis
 from .import bitcoin
 from .bitcoin import is_address,  hash_160, COIN, TYPE_ADDRESS
 from .transaction import Transaction
@@ -43,6 +43,7 @@ from .import paymentrequest
 from .paymentrequest import PR_PAID, PR_UNPAID, PR_UNKNOWN, PR_EXPIRED
 from .import contacts
 from .plugins import run_hook
+from .i18n import _
 
 known_commands = {}
 
@@ -655,12 +656,12 @@ class Commands:
     def notify(self, address, URL):
         """Watch an address. Everytime the address changes, a http POST is sent to the URL."""
         def callback(x):
-            import urllib2
+            import urllib.request
             headers = {'content-type':'application/json'}
             data = {'address':address, 'status':x.get('result')}
             try:
-                req = urllib2.Request(URL, json.dumps(data), headers)
-                response_stream = urllib2.urlopen(req, timeout=5)
+                req = urllib.request.Request(URL, json.dumps(data), headers)
+                response_stream = urllib.request.urlopen(req, timeout=5)
                 util.print_error('Got Response for %s' % address)
             except BaseException as e:
                 util.print_error(str(e))
