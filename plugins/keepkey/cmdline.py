e6a001da14109cd9f6a8def7958b4b9dad54049e
CodeFace <i@codeface.cn>
2017-12-14 14:06:48 +0800

    Fix undefined reference error in command line KeepKey plugin. follow https://github.com/spesmilo/electrum/commit/8cb34f4d88184d0c42e8c1fc41f451fa3cd5a6be

--- plugins/keepkey/cmdline.py
+++ plugins/keepkey/cmdline.py
@@ -2,6 +2,8 @@ from electrum.plugins import hook
 from electrum.util import print_msg
 from .keepkey import KeepKeyPlugin
 from ..hw_wallet import CmdLineHandler
+from electrum.plugins import hook
+
 
 class Plugin(KeepKeyPlugin):
     handler = CmdLineHandler()
