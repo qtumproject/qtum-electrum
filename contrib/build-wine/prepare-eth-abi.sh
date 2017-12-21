fe646fd123bbb08082d31665c3ba7915857b5e33
CodeFace <i@codeface.cn>
2017-12-21 15:16:34 +0800

    fix prepare-eth-abi

--- contrib/build-wine/prepare-eth-abi.sh
+++ contrib/build-wine/prepare-eth-abi.sh
@@ -9,13 +9,29 @@ export WINEPREFIX=/opt/wine64
 PYHOME=c:/python$PYTHON_VERSION
 PYTHON="wine $PYHOME/python.exe -OO -B"
 
-# Let's begin!
 cd `dirname $0`
 set -e
-
 cd tmp
+if [ ! -d "eth-utils" ]; then
+    git clone -b $BRANCH $ETH_UTILS_GIT_URL eth-utils
+fi
+if [ ! -d "eth-abi" ]; then
+    git clone -b $BRANCH $ETH_ABI_GIT_URL eth-abi
+fi
 
-# Install
-$PYTHON -m pip install git+ETH_UTILS_GIT_URL
-$PYTHON -m pip install git+ETH_ABI_GIT_URL
+pushd eth-utils
+git pull
+git checkout $BRANCH
+$PYTHON setup.py install
+popd
+
+pushd eth-abi
+git pull
+git checkout $BRANCH
+$PYTHON setup.py install
+popd
+
+
+
+
 
