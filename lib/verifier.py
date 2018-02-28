6dea4cb392ecf8684312fea1b15e9240472ee793
CodeFace <i@codeface.cn>
2018-02-28 11:42:04 +0800

    update verifier

--- lib/verifier.py
+++ lib/verifier.py
@@ -36,16 +36,31 @@ class SPV(ThreadJob):
         self.merkle_roots = {}
 
     def run(self):
+        interface = self.network.interface
+        if not interface:
+            return
+        blockchain = interface.blockchain
+        if not blockchain:
+            return
+
         lh = self.network.get_local_height()
         unverified = self.wallet.get_unverified_txs()
+
         for tx_hash, tx_height in unverified.items():
             # do not request merkle branch before headers are available
             if (tx_height > 0) and (tx_hash not in self.merkle_roots) and (tx_height <= lh):
-                request = ('blockchain.transaction.get_merkle',
-                           [tx_hash, tx_height])
-                self.network.send([request], self.verify_merkle)
-                self.print_error('requested merkle', tx_hash)
-                self.merkle_roots[tx_hash] = None
+                header = blockchain.read_header(tx_height)
+                if header is None:
+                    index = tx_height // 2016
+                    if index < len(blockchain.checkpoints):
+                        self.network.request_chunk(interface, index)
+                else:
+                    if tx_hash not in self.merkle_roots:
+                        request = ('blockchain.transaction.get_merkle',
+                                   [tx_hash, tx_height])
+                        self.network.send([request], self.verify_merkle)
+                        self.print_error('requested merkle', tx_hash)
+                        self.merkle_roots[tx_hash] = None
 
         if self.network.blockchain() != self.blockchain:
             self.blockchain = self.network.blockchain()
