15f77a22bd04d5fd37a60561c96a57deb72e1daf
CodeFace <i@codeface.cn>
2018-03-07 15:13:50 +0800

    fix msg sign and verify magic

--- lib/qtum.py
+++ lib/qtum.py
@@ -709,7 +709,7 @@ from ecdsa.util import string_to_number, number_to_string
 
 def msg_magic(message):
     length = bfh(var_int(len(message)))
-    return b"\x18Qtum Signed Message:\n" + length + message
+    return b"\x15Qtum Signed Message:\n" + length + message
 
 
 def verify_message(address, sig, message):
