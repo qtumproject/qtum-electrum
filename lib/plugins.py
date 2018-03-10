5a61f97142895e90e58ed29222d7a27aa17f58a7
CodeFace <i@codeface.cn>
2018-03-10 19:16:21 +0800

    fix crash when multiple trezors connected with one being wiped follow https://github.com/spesmilo/electrum/commit/9372c31b5d3ba7fdb0caace5643d96f0fb7ff6e8

--- lib/plugins.py
+++ lib/plugins.py
@@ -496,7 +496,8 @@ class DeviceMgr(ThreadJob, PrintError):
             if info.label == keystore.label:
                 return info
         msg = _("Please select which %s device to use:") % plugin.device
-        descriptions = [info.label + ' (%s)'%(_("initialized") if info.initialized else _("wiped")) for info in infos]
+        descriptions = [str(info.label) + ' (%s)' % (_("initialized") if info.initialized else _("wiped")) for info in
+                        infos]
         c = handler.query_choice(msg, descriptions)
         if c is None:
             raise UserCancelled()
