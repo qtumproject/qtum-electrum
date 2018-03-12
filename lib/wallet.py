85e9e03491ddbcc3c86618670c5e4655f709f328
CodeFace <i@codeface.cn>
2018-03-12 11:31:13 +0800

    fix some storage crash case follow https://github.com/spesmilo/electrum/commit/a048a00594f00da0cd800b9b25a8a2cde341db1e

--- lib/wallet.py
+++ lib/wallet.py
@@ -2171,5 +2171,4 @@ class Wallet(object):
             return Multisig_Wallet
         if wallet_type in wallet_constructors:
             return wallet_constructors[wallet_type]
-        raise RuntimeError("Unknown wallet type: " + wallet_type)
-
+        raise RuntimeError("Unknown wallet type: " + str(wallet_type))
