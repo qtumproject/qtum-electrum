d1f4a07af3dbe289d59f3463ea7cfedfd1814bc0
CodeFace <i@codeface.cn>
2018-03-22 14:37:42 +0800

    eth-hash

--- contrib/build-wine/prepare-eth-abi.sh
+++ contrib/build-wine/prepare-eth-abi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PYTHON_VERSION=3.5.4
 
+ETH_HASH_GIT_URL=https://github.com/icodeface/eth-hash
 ETH_UTILS_GIT_URL=https://github.com/icodeface/eth-utils
 ETH_ABI_GIT_URL=https://github.com/icodeface/eth-abi
 BRANCH=master
@@ -9,17 +10,27 @@ export WINEPREFIX=/opt/wine64
 PYHOME=c:/python$PYTHON_VERSION
 PYTHON="wine $PYHOME/python.exe -OO -B"
 
-$PYTHON -m pip install eth-hash
+$PYTHON -m pip uninstall eth-hash
+$PYTHON -m pip uninstall eth-hash[pycryptodome]
 
 cd `dirname $0`
 set -e
 cd tmp
+if [ ! -d "eth-hash" ]; then
+    git clone -b $BRANCH $ETH_HASH_GIT_URL eth-hash
+fi
 if [ ! -d "eth-utils" ]; then
     git clone -b $BRANCH $ETH_UTILS_GIT_URL eth-utils
 fi
 if [ ! -d "eth-abi" ]; then
     git clone -b $BRANCH $ETH_ABI_GIT_URL eth-abi
 fi
+
+pushd eth-hash
+git pull
+git checkout $BRANCH
+$PYTHON setup.py install
+popd
 
 pushd eth-utils
 git pull
