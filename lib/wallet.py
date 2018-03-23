36e3263b7abfde9a71a12d176d7eef93bb1e920d
CodeFace <i@codeface.cn>
2018-03-23 13:00:11 +0800

    add_payment_request check addr follow https://github.com/spesmilo/electrum/commit/800c783580ca2cff6d8ac380f66e04f059026355

--- lib/wallet.py
+++ lib/wallet.py
@@ -1482,6 +1482,10 @@ class Abstract_Wallet(PrintError):
 
     def add_payment_request(self, req, config):
         addr = req['address']
+        if not bitcoin.is_address(addr):
+            raise Exception(_('Invalid Bitcoin address.'))
+        if not self.is_mine(addr):
+            raise Exception(_('Address not in wallet.'))
         amount = req.get('amount')
         message = req.get('memo')
         self.receive_requests[addr] = req
