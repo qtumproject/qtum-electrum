dedcb130f5bb63155f076991cbd20a7a0d5c7a79
CodeFace <i@codeface.cn>
2018-03-26 10:46:21 +0800

    fix paying to script follow https://github.com/spesmilo/electrum/commit/05342c553727707a25641b64dcb78f748cfd3e70

--- gui/qt/paytoedit.py
+++ gui/qt/paytoedit.py
@@ -30,6 +30,7 @@ from .qrtextedit import ScanQRTextEdit
 import re
 from decimal import Decimal
 from qtum_electrum import bitcoin
+from qtum_electrum.util import bfh
 
 from . import util
 
@@ -92,9 +93,14 @@ class PayToEdit(ScanQRTextEdit):
         for word in x.split():
             if word[0:3] == 'OP_':
                 assert word in opcodes.lookup
-                script += chr(opcodes.lookup[word])
+                # script += chr(opcodes.lookup[word])
+                opcode_int = opcodes.lookup[word]
+                assert opcode_int < 256  # opcode is single-byte
+                script += bitcoin.int_to_hex(opcode_int)
             else:
-                script += push_script(word).decode('hex')
+                # script += push_script(word).decode('hex')
+                bfh(word)  # to test it is hex data
+                script += push_script(word)
         return script
 
     def parse_amount(self, x):
