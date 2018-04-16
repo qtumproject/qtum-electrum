9a1e85e8c0cd1b37d82f6773d41eeccadfca8d0f
codeface <i@codeface.cn>
2018-04-16 14:32:30 +0800

    fix https://github.com/spesmilo/electrum/issues/4236 follow https://github.com/spesmilo/electrum/commit/4ff3791cb93fcdda0b534bc2be575121f4d59705

--- lib/transaction.py
+++ lib/transaction.py
@@ -821,19 +821,18 @@ class Transaction:
         return len(signatures) == num_sig
 
     @classmethod
-    def get_preimage_script(self, txin):
-        # only for non-segwit
+    def get_preimage_script(cls, txin):
+        pubkeys, x_pubkeys = cls.get_sorted_pubkeys(txin)
         if txin['type'] == 'p2pkh':
             return bitcoin.address_to_script(txin['address'])
         elif txin['type'] in ['p2sh', 'p2wsh', 'p2wsh-p2sh']:
-            pubkeys, x_pubkeys = self.get_sorted_pubkeys(txin)
             return multisig_script(pubkeys, txin['num_sig'])
         elif txin['type'] in ['p2wpkh', 'p2wpkh-p2sh']:
-            pubkey = txin['pubkeys'][0]
+            pubkey = pubkeys[0]
             pkh = bh2u(bitcoin.hash_160(bfh(pubkey)))
             return '76a9' + push_script(pkh) + '88ac'
         elif txin['type'] == 'p2pk':
-            pubkey = txin['pubkeys'][0]
+            pubkey = pubkeys[0]
             return bitcoin.public_key_to_p2pk_script(pubkey)
         else:
             raise TypeError('Unknown txin type', txin['type'])
