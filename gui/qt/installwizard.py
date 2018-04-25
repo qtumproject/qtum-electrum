fe515302347ad83d696d41fee28dad6e8fd752d8
codeface <i@codeface.cn>
2018-04-25 23:12:08 +0800

    seed ext enable

--- gui/qt/installwizard.py
+++ gui/qt/installwizard.py
@@ -394,17 +394,17 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
 
     @wizard_dialog
     def restore_seed_dialog(self, run_next, test):
-        options = []
-        # if self.opt_ext:
-        #     options.append('ext')
-        # if self.opt_bip39:
-        #     options.append('bip39')
         title = _('Enter Seed')
+        options = []
         if 'mobile' == self.wallet_type:
             message = ''.join([
                 _('Please enter your seed phrase in order to restore your wallet. \n'),
                 _('This is compatible with qtum mobile wallet. \n')])
         else:
+            if self.opt_ext:
+                options.append('ext')
+            # if self.opt_bip39:
+            #     options.append('bip39')
             message = ''.join([
                 _('Please enter your seed phrase in order to restore your wallet. \n'),
                 _('Qtum Electrum is not compatiable with qtum mobile wallet seed words yet. \n'),
