c7009b0cfef9005e549fea763932d0d086aba119
codeface <i@codeface.cn>
2018-05-02 17:57:09 +0800

    hw_wallet code clean up

--- plugins/hw_wallet/cmdline.py
+++ plugins/hw_wallet/cmdline.py
@@ -2,17 +2,21 @@ from qtum_electrum.util import print_msg, print_error
 
 
 class CmdLineHandler:
+
     def get_passphrase(self, msg, confirm):
         import getpass
         print_msg(msg)
         return getpass.getpass('')
 
     def get_pin(self, msg):
-        t = {'a': '7', 'b': '8', 'c': '9', 'd': '4', 'e': '5', 'f': '6', 'g': '1', 'h': '2', 'i': '3'}
+        t = { 'a':'7', 'b':'8', 'c':'9', 'd':'4', 'e':'5', 'f':'6', 'g':'1', 'h':'2', 'i':'3'}
         print_msg(msg)
         print_msg("a b c\nd e f\ng h i\n-----")
         o = input()
-        return ''.join(map(lambda x: t[x], o))
+        try:
+            return ''.join(map(lambda x: t[x], o))
+        except KeyError as e:
+            raise Exception("Character {} not in matrix!".format(e)) from e
 
     def show_error(self, msg, blocking=False):
         print_msg(msg)
@@ -36,7 +40,7 @@ class CmdLineHandler:
         print_msg(msg)
 
     def update_status(self, b):
-        print_error('trezor status', b)
+        print_error('hw device status', b)
 
     def finished(self):
         pass
