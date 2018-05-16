66c0452b86c4ff42038e6242ec72e6abb05d062c
codeface <i@codeface.cn>
2018-05-16 15:42:51 +0800

    Trezor: Matrix recovery support, follow https://github.com/spesmilo/electrum/commit/6dd516172903c7029bb25cc1ab3671968470d1fb and https://github.com/spesmilo/electrum/commit/809d6aefcacf5b7b8244bee3e62763715c0a7ac2 and https://github.com/spesmilo/electrum/commit/364cfd17aec25a751e559c1166773792aba1e8d2

--- plugins/trezor/clientbase.py
+++ plugins/trezor/clientbase.py
@@ -83,6 +83,15 @@ class GuiMixin(object):
         return self.proto.PassphraseStateAck()
 
     def callback_WordRequest(self, msg):
+        if (msg.type is not None
+            and msg.type in (self.types.WordRequestType.Matrix9,
+                             self.types.WordRequestType.Matrix6)):
+            num = 9 if msg.type == self.types.WordRequestType.Matrix9 else 6
+            char = self.handler.get_matrix(num)
+            if char == 'x':
+                return self.proto.Cancel()
+            return self.proto.WordAck(word=char)
+
         self.step += 1
         msg = _("Step %d/24.  Enter seed word as explained on "
                 "your %s:") % (self.step, self.device)
