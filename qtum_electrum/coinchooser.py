5841f556b941708cacbf1f46c5fd07cefb505c9e
codeface <i@codeface.cn>
2018-08-08 17:24:45 +0800

    coinchoose: small optimisations

--- qtum_electrum/coinchooser.py
+++ qtum_electrum/coinchooser.py
@@ -81,13 +81,14 @@ def strip_unneeded(bkts, sufficient_funds, exception_addr=None) -> list:
     '''Remove buckets that are unnecessary in achieving the spend amount'''
     bkts = sorted(bkts, key=lambda bkt: bkt.value)
 
-    # move the exception bucket to the end
-    for i in range(len(bkts)):
-        if exception_addr and bkts[i].desc == exception_addr:
-            exception_bucket = bkts[i]
-            del bkts[i]
-            bkts.append(exception_bucket)
-            break
+    if exception_addr:
+        # move the exception bucket to the end
+        for i in range(len(bkts)):
+            if bkts[i].desc == exception_addr:
+                exception_bucket = bkts[i]
+                del bkts[i]
+                bkts.append(exception_bucket)
+                break
 
     for i in range(len(bkts)):
         if not sufficient_funds(bkts[i + 1:]):
