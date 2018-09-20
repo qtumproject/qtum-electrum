7c0f33db5549cb21b1b9b0619aa4860fb1117c9a
codeface <i@codeface.cn>
2018-09-20 16:00:48 +0800

    verifier: perf optimisations

--- qtum_electrum/verifier.py
+++ qtum_electrum/verifier.py
@@ -56,18 +56,20 @@ class SPV(ThreadJob):
         unverified = self.wallet.get_unverified_txs()
 
         for tx_hash, tx_height in unverified.items():
-            # do not request merkle branch before headers are available
+            # do not request merkle branch if we already requested it
+            if tx_hash in self.requested_merkle or tx_hash in self.merkle_roots:
+                continue
+            # or before headers are available
             if (tx_height > 0) and (tx_hash not in self.merkle_roots) and (tx_height <= lh):
                 header = blockchain.read_header(tx_height)
                 if header is None:
                     index = tx_height // 2016
                     self.network.request_chunk(interface, index)
-                else:
-                    if (tx_hash not in self.requested_merkle
-                            and tx_hash not in self.merkle_roots):
-                        self.network.get_merkle_for_transaction(tx_hash, tx_height, self.verify_merkle)
-                        self.print_error('requested merkle', tx_hash)
-                        self.requested_merkle.add(tx_hash)
+                    continue
+                # request now
+                self.network.get_merkle_for_transaction(tx_hash, tx_height, self.verify_merkle)
+                self.print_error('requested merkle', tx_hash)
+                self.requested_merkle.add(tx_hash)
 
         if self.network.blockchain() != self.blockchain:
             self.blockchain = self.network.blockchain()
