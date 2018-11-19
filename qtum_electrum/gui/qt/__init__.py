1f6dc9700a2efffc2fad6b177ea3e9d3b1b0cbc2
codeface <i@codeface.cn>
2018-11-19 15:12:25 +0800

    [follow] qt: count wizards in progress follow https://github.com/spesmilo/electrum/commit/4d62963efe79d2224a5a273b3eaac207def68a7b

--- qtum_electrum/gui/qt/__init__.py
+++ qtum_electrum/gui/qt/__init__.py
@@ -27,6 +27,7 @@ import signal
 import sys
 import traceback
 import locale
+import threading
 
 try:
     import PyQt5
@@ -106,6 +107,8 @@ class ElectrumGui:
         self.timer = Timer()
         self.nd = None
         self.network_updated_signal_obj = QNetworkUpdatedSignalObject()
+        self._num_wizards_in_progress = 0
+        self._num_wizards_lock = threading.Lock()
         # init tray
         self.dark_icon = self.config.get("dark_icon", False)
         self.tray = QSystemTrayIcon(self.tray_icon(), None)
@@ -183,6 +186,18 @@ class ElectrumGui:
         run_hook('on_new_window', w)
         return w
 
+    def count_wizards_in_progress(func):
+        def wrapper(self: 'ElectrumGui', *args, **kwargs):
+            with self._num_wizards_lock:
+                self._num_wizards_in_progress += 1
+            try:
+                return func(self, *args, **kwargs)
+            finally:
+                with self._num_wizards_lock:
+                    self._num_wizards_in_progress -= 1
+        return wrapper
+
+    @count_wizards_in_progress
     def start_new_window(self, path, uri, app_is_starting=False):
         '''Raises the window for the wallet if it is open.  Otherwise
         opens the wallet and creates a new window for it'''
@@ -279,11 +294,15 @@ class ElectrumGui:
         signal.signal(signal.SIGINT, lambda *args: self.app.quit())
 
         def quit_after_last_window():
-            # on some platforms, not only does exec_ not return but not even
-            # aboutToQuit is emitted (but following this, it should be emitted)
-            if self.app.quitOnLastWindowClosed():
-                self.app.quit()
-
+            # keep daemon running after close
+            if self.config.get('daemon'):
+                return
+            # check if a wizard is in progress
+            with self._num_wizards_lock:
+                if self._num_wizards_in_progress > 0 or len(self.windows) > 0:
+                    return
+            self.app.quit()
+        self.app.setQuitOnLastWindowClosed(False)  # so _we_ can decide whether to quit
         self.app.lastWindowClosed.connect(quit_after_last_window)
 
         def clean_up():
