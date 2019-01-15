a54f2f1080c6f2e3f19748905107dc7adabbb632
codeface <i@codeface.cn>
2019-01-15 10:15:13 +0800

    [follow] qt: don't import PyQt5.Qt follow https://github.com/spesmilo/electrum/commit/bd1f7b539e09d77c75baa7512bd8fec1dedc5f47#diff-17adebe9250a23504307b8d80ab9d5b3

--- qtum_electrum/plugins/hw_wallet/qt.py
+++ qtum_electrum/plugins/hw_wallet/qt.py
@@ -26,7 +26,7 @@
 
 import threading
 
-from PyQt5.Qt import QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QVBoxLayout, QLabel
 from qtum_electrum.gui.qt.password_dialog import PasswordLayout, PW_PASSPHRASE
 from qtum_electrum.gui.qt.util import *
 
