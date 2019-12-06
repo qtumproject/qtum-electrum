4dbad3b853cfaab8e438c7964783b78915a70c26
CodeFace <cnchenjun@qq.com>
2019-12-06 16:56:35 +0800

    fix CoinChooserQtum duplicated txin

--- electrum/coinchooser.py
+++ electrum/coinchooser.py
@@ -478,11 +478,13 @@ class CoinChooserQtum(CoinChooserPrivacy):
     def make_tx(self, *, coins: Sequence[PartialTxInput], inputs: List[PartialTxInput],
                 outputs: List[PartialTxOutput], change_addrs: Sequence[str],
                 fee_estimator_vb: Callable, dust_threshold: int, sender=None):
+        coins = list(coins)
         if sender is not None:
             found = False
-            for coin in coins:
+            for index, coin in enumerate(coins):
                 if coin.address == sender:
                     inputs.insert(0, coin)
+                    del coins[index]
                     found = True
                     break
             if not found:
