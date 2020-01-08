eaeeebcae496493a7838bfb1b64edb0d7689ad1c
CodeFace <cnchenjun@qq.com>
2020-01-08 13:56:48 +0800

    ledger: fix signing contract tx

--- electrum/plugins/ledger/ledger.py
+++ electrum/plugins/ledger/ledger.py
@@ -382,12 +382,14 @@ class Ledger_KeyStore(Hardware_KeyStore):
         if not client_electrum.supports_multi_output():
             if len(tx.outputs()) > 2:
                 self.give_error("Transaction with more than 2 outputs not supported")
-        for txout in tx.outputs():
-            if not txout.address:
-                if client_electrum.is_hw1():
-                    self.give_error(_("Only address outputs are supported by {}").format(self.device))
-                # note: max_size based on https://github.com/LedgerHQ/ledger-app-btc/commit/3a78dee9c0484821df58975803e40d58fbfc2c38#diff-c61ccd96a6d8b54d48f54a3bc4dfa7e2R26
-                validate_op_return_output(txout, max_size=190)
+
+        # don't restrict tx output
+        # for txout in tx.outputs():
+        #     if not txout.address:
+        #         if client_electrum.is_hw1():
+        #             self.give_error(_("Only address outputs are supported by {}").format(self.device))
+        #         # note: max_size based on https://github.com/LedgerHQ/ledger-app-btc/commit/3a78dee9c0484821df58975803e40d58fbfc2c38#diff-c61ccd96a6d8b54d48f54a3bc4dfa7e2R26
+        #         validate_op_return_output(txout, max_size=190)
 
         # Output "change" detection
         # - only one output and one change is authorized (for hw.1 and nano)
