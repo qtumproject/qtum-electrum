1f2d9ca4fbacea4c93f82c5ee0e94e1a3a854adc
codeface <i@codeface.cn>
2020-07-13 23:39:30 +0800

    remove os name from binary name

--- contrib/build-linux/appimage/build.sh
+++ contrib/build-linux/appimage/build.sh
@@ -19,7 +19,7 @@ SQUASHFSKIT_COMMIT="ae0d656efa2d0df2fcac795b6823b44462f19386"
 
 
 VERSION=`git describe --tags --dirty --always`
-APPIMAGE="$DISTDIR/Qtum-electrum-linux-$VERSION-x86_64.AppImage"
+APPIMAGE="$DISTDIR/Qtum-electrum-$VERSION-x86_64.AppImage"
 
 . "$CONTRIB"/build_tools_util.sh
 
