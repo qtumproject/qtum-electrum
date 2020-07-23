a50e7bb134e4b3b464804c31341a18c1faa69245
codeface <i@codeface.cn>
2020-07-23 13:39:26 +0800

    prefer legacy seed type

--- electrum/base_wizard.py
+++ electrum/base_wizard.py
@@ -709,8 +709,8 @@ class BaseWizard(Logger):
             ])
         if choices is None:
             choices = [
-                ('create_segwit_seed', _('Segwit')),
                 ('create_standard_seed', _('Legacy')),
+                ('create_segwit_seed', _('Segwit')),
             ]
         self.choice_dialog(title=title, message=message, choices=choices, run_next=self.run)
 
