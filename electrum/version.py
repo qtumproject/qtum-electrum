a4efb23a742e116ea583284409aaa474709f6589
codeface <i@codeface.cn>
2020-08-31 22:25:05 +0800

    v4.0.9

--- electrum/version.py
+++ electrum/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = '4.0.8'     # version of the client package
+ELECTRUM_VERSION = '4.0.9'     # version of the client package
 APK_VERSION = '4.0.2.0'        # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.5'     # protocol version requested
